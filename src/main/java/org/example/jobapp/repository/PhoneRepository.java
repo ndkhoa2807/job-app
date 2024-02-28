bba8095b4befe89385699be8f2df4084d8f51e6f
Khoa Nguyen <khoand2807@gmail.com>
2024-02-28 22:20:35 +0700

    edit keycloak config

--- src/main/java/org/example/jobapp/repository/PhoneRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.jobapp.repository;
-
-import org.example.jobapp.model.Phone;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PhoneRepository extends JpaRepository<Phone, Long> {
-}
