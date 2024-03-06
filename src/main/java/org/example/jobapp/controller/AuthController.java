5af99bb39ffb57151b43dd0b244567adaf107584
Nguyen Dang Khoa <khoa.nguyendang1@mobifone.vn>
2024-03-06 14:23:52 +0700

    remove infinispan and add logout endpoint

--- src/main/java/org/example/jobapp/controller/AuthController.java
+++ src/main/java/org/example/jobapp/controller/AuthController.java
@@ -1,19 +1,20 @@
 package org.example.jobapp.controller;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.jobapp.dto.CommonResponse;
 import org.example.jobapp.dto.User;
 import org.example.jobapp.keycloakclient.UserService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.ZonedDateTime;
+
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -21,6 +22,11 @@ import java.time.ZonedDateTime;
 @RequiredArgsConstructor
 public class AuthController {
     private final UserService userService;
+
+    @Value("${security.logout-endpoint}")
+    private String LOGOUT_URL;
+    @Value("${security.logout-redirect}")
+    private String REDIRECT_URI;
 
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@RequestBody @Valid User user) {
@@ -31,6 +37,13 @@ public class AuthController {
                 .message("Successfully create user")
                 .build();
         return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request) throws ServletException {
+        request.logout();
+        StringBuilder stringBuilder = new StringBuilder();
+        return stringBuilder.append("redirect:").append(LOGOUT_URL).append("?redirect_uri=").append(REDIRECT_URI).toString();
     }
 
 
